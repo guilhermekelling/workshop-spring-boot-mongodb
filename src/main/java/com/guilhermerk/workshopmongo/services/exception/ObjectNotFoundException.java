88bb651bd314bf52e21e857c21cf96359765911b
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2019-09-10 13:56:26 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/guilhermerk/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.guilhermerk.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
