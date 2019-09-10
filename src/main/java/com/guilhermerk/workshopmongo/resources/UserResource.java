f995b465f98bcd2a31edd0bebd967bf44fcb90f4
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2019-09-10 11:20:08 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/guilhermerk/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guilhermerk/workshopmongo/resources/UserResource.java
@@ -4,23 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilhermerk.workshopmongo.domain.User;
+import com.guilhermerk.workshopmongo.service.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
  	public ResponseEntity<List<User>> findAll() {
-		User guilherme = new User("1", "Guilherme Kelling", "guilhermekelling@gmail.com");
-		User renato = new User("2", "Alex Green", "guilherme@grupocpa.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(guilherme, renato));
+		List<User> list = service.findAll();
+		
 		return ResponseEntity.ok().body(list);
 	}
 }
